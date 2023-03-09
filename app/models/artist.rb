528bb1fdbffd82aaefca0b05db85e623b7cf220e
Nick Scofield <c.nick.scofield@gmail.com>
2023-03-09 15:20:29 -0700

    Adjusted validations for Artist model.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ApplicationRecord
   has_many :tracks, dependent: :destroy
   has_many :albums, through: :tracks
+
+  validates :name, presence: true
+  validates :image, presence: true
 end
