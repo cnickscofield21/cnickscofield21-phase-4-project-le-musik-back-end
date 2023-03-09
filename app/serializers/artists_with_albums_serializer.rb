f2ebcb73a529a4432567a5dec7a67726a1edb801
Nick Scofield <c.nick.scofield@gmail.com>
2023-03-09 14:55:38 -0700

    Improved the data returned for artists

--- app/serializers/artists_with_albums_serializer.rb
+++ app/serializers/artists_with_albums_serializer.rb
@@ -1,4 +1,12 @@
 class ArtistsWithAlbumsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image
-  has_many :albums
+  attributes :id, :name, :image, :albums
+
+  # Sends track's album for every track
+  # has_many :albums
+
+  # Sends full albums object including timestamps
+  def albums
+    object.albums.uniq
+  end
+
 end
