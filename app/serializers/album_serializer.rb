fb3bf9b7bd09bb8e09c22d30ea06963d68b321a6
Josiah Storrie <jpstorrie@gmail.com>
2023-03-10 12:02:38 -0700

    push right before project

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,5 +1,9 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :image
+  attributes :id, :title, :year, :image, :artist_id
   has_many :tracks
+
+  def artist_id
+    object.artists.first.id
+  end
 
 end
--- app/serializers/albums_with_artists_serializer.rb
+++ app/serializers/albums_with_artists_serializer.rb
@@ -2,8 +2,4 @@ class AlbumsWithArtistsSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :image
   has_many :tracks
 
-  def artist_id
-    object.artists.first.id
-  end
-
 end
