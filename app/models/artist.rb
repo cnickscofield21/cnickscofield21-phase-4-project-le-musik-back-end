ad0fcdb10cb47b38485cdf40aa6b19d23d3a4d61
Nick Scofield <c.nick.scofield@gmail.com>
2023-03-10 09:48:28 -0700

    Alters validations & authorization requirements

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,6 @@ class Artist < ApplicationRecord
   has_many :tracks, dependent: :destroy
   has_many :albums, through: :tracks
 
-  validates :name, presence: true
-  validates :image, presence: true
+  # validates :name, presence: true
+  # validates :image, presence: true
 end
